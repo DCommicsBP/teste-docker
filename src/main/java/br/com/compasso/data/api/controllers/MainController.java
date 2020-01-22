4405eb3a64cbba70691abcf8631b9efa17766e79
DCommicsBP <dayonne.p@gmail.com>
2020-01-22 16:37:46 -0300

    adicionando config

--- src/main/java/br/com/compasso/data/api/controllers/MainController.java
+++ src/main/java/br/com/compasso/data/api/controllers/MainController.java
@@ -1,6 +1,8 @@
 package br.com.compasso.data.api.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +17,18 @@ public class MainController {
 	public String teste02() {
 		return  "Ola 2"; 
 	}
+	
+	  @Value("${greeter.message}")
+	    private String greeterMessageFormat; 
+	 
+	    @GetMapping("/greet/{user}")
+	    public String greet(@PathVariable("user") String user) {
+	        String prefix = System.getenv().getOrDefault("GREETING_PREFIX", "Hi");
+	        System.out.println("Prefix "+prefix+"and User:"+ user+"");
+	        if (prefix == null) {
+	            prefix = "Hello!";
+	        }
+	 
+	        return String.format(greeterMessageFormat, prefix, user);
+	    }
 }
